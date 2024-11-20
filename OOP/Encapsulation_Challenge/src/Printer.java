72ac56d6750086cf283cea46822377922ba6d222
Dominique Mariacher <dmariacher11@gmail.com>
2024-11-20 22:35:49 +0100

    some OOP stuff -> Encapsulation (some small projects)

---
+++ OOP/Encapsulation_Challenge/src/Printer.java
@@ -0,0 +1,37 @@
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.pagesPrinted = 0;
+        this.tonerLevel = tonerLevel >= 0 && tonerLevel <= 100 ? tonerLevel : -1;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+
+        int tempAmount = tonerAmount + tonerLevel;
+        if (tempAmount > 100 || tempAmount < 0) {
+            return -1;
+        }
+        tonerLevel += tonerAmount;
+        return tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        if (pages <= 0) {
+            return 0;
+        }
+        int jobPages = (duplex) ? (pages / 2) + (pages % 2) : pages;
+        System.out.println(duplex ? "printing in duplex mode" : "printing not in duplex mode");
+        pagesPrinted += jobPages;
+        return jobPages;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+}
