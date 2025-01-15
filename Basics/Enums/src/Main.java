7924c077215ac9a98b5e5034eec2b69d4cd12bf0
Dominique Mariacher <dmariacher11@gmail.com>
2025-01-15 20:31:29 +0100

    Arraylist/LinkedList/Abstraction continue...

---
+++ Basics/Enums/src/Main.java
@@ -0,0 +1,30 @@
+import dev.lpa.DayOfTheWeek;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DayOfTheWeek weekDay = DayOfTheWeek.TUE;
+        System.out.println(weekDay);
+
+        for (int i = 0; i < 10; i++) {
+            weekDay = getRandomDay();
+            System.out.printf("Name is %s, Ordinal Value = %d%n", weekDay.name(), weekDay.ordinal());
+
+            if (weekDay == DayOfTheWeek.FRI) {
+                System.out.println("Found a Friday!!!");
+            }
+        }
+    }
+
+    public static DayOfTheWeek getRandomDay() {
+
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
+    }
+
+
+}
---
+++ Basics/Enums/src/dev/lpa/DayOfTheWeek.java
@@ -0,0 +1,7 @@
+package dev.lpa;
+
+public enum DayOfTheWeek {
+
+    SUN,MON,TUE,WED,THU,FRI,SAT;
+
+}
